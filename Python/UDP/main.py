65db5a514d3b8fe57641602a25f41523134e1461
Papa Smerf <albert.lis@interia.pl>
2019-05-28 19:57:19 +0200

    Optimize python Code

--- Python/UDP/main.py
+++ Python/UDP/main.py
@@ -1,19 +1,12 @@
 import socket
-import time
 
-UDP_IP = "192.168.137.186"
-UDP_PORT = 4210
-IP_R = "127.0.0.1"
+IP_R = "0.0.0.0"
 PORT_R = 5005
-MESSAGE = "abc"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock1.bind(('', PORT_R))
+sock.bind((IP_R, PORT_R))
 
-#sock = UDP.MulticastListener(UDP_IP,UDP_PORT)
 while True:
-    sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
-    data, adrr = sock1.recvfrom(128)
+    data, adrr = sock.recvfrom(128)
+    data = data[:-2]
     print(data)
-    time.sleep(1)
